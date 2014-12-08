b738928a27a6a09ec2aadfb83fcb0fdc866e6db7
vodoman123 <harsha_bande@yahoo.com>
2014-12-08 17:43:57 -0600

    allow for user to slow down and speed up audio

--- udacity_swift_course/PitchPerfect/PitchPerfect/PlaySoundsViewController.swift
+++ udacity_swift_course/PitchPerfect/PitchPerfect/PlaySoundsViewController.swift
@@ -12,12 +12,13 @@ import AVFoundation
 class PlaySoundsViewController: UIViewController {
     
     var audioPlayer:AVAudioPlayer!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         if var filePath = NSBundle.mainBundle().pathForResource("movie_quote", ofType: "mp3"){
             var filePathUrl = NSURL.fileURLWithPath(filePath)
             audioPlayer = AVAudioPlayer(contentsOfURL: filePathUrl, error: nil)
+            audioPlayer.enableRate = true
             
         }else{
             println("file path is empty")
@@ -31,9 +32,22 @@ class PlaySoundsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    @IBAction func slowSound(sender: UIButton) {
+    @IBAction func fastSound(sender: UIButton) {
+        audioPlayer.stop()
+        audioPlayer.currentTime = 0.0
+        audioPlayer.rate = 1.5
         audioPlayer.play()
+    }
+    
+    @IBAction func slowSound(sender: UIButton) {
+        audioPlayer.stop()
+        audioPlayer.currentTime = 0.0
+        audioPlayer.rate = 0.5
+        audioPlayer.play()
+    }
+    
+    @IBAction func stopSound(sender: UIButton) {
+        audioPlayer.stop()
     }
     /*
     // MARK: - Navigation
